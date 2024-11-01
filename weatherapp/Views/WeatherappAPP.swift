caa4744ff30e6137874fc6fc33bb1d39ea1307ea
Advait Singh <Advaitsingh05@gmail.com>
2024-11-01 18:48:37 +0530

    Initial Commit

---
+++ weatherapp/Views/WeatherappAPP.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherappAPP.swift
+//  weatherapp
+//
+//  Created by Advait Singh on 22/06/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
